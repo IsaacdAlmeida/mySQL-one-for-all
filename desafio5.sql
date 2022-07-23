5207510304d39b3baae4e03634cdd82ecbaa1348
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-07-23 17:59:45 -0300

    feat(mySQL): show the two most played songs in alphabetical order

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+SELECT 
+	s.song_name AS cancao,
+    count(s.song_id) AS reproducoes
+FROM SpotifyClone.songs AS s
+INNER JOIN 
+	SpotifyClone.users_history AS h ON s.song_id = h.song_id
+GROUP BY s.song_name
+ORDER BY reproducoes DESC, cancao
+LIMIT 2;
